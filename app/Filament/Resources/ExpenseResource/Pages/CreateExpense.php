1bb2f2c82ba455764d5c927cf71c8dcdb6cc37f6
ranggautama47 <ranggautama737@gmail.com>
2026-05-17 21:13:07 +0700

    feat: add ExpenseResource with category filter and date range

---
+++ app/Filament/Resources/ExpenseResource/Pages/CreateExpense.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Filament\Resources\ExpenseResource\Pages;
+
+use App\Filament\Resources\ExpenseResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateExpense extends CreateRecord
+{
+    protected static string $resource = ExpenseResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function getCreatedNotificationTitle(): ?string
+    {
+        return 'Expense berhasil dicatat';
+    }
+}
---
+++ app/Filament/Resources/ExpenseResource/Pages/EditExpense.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Filament\Resources\ExpenseResource\Pages;
+
+use App\Filament\Resources\ExpenseResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditExpense extends EditRecord
+{
+    protected static string $resource = ExpenseResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+            Actions\RestoreAction::make(),
+        ];
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function getSavedNotificationTitle(): ?string
+    {
+        return 'Expense berhasil diupdate';
+    }
+}
