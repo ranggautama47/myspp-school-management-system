d4da2965011ff47234ed09f1330b07d8f705abd9
ranggautama47 <ranggautama737@gmail.com>
2026-05-19 15:02:47 +0700

    feat: add Midtrans sandbox config to .env.example

--- config/services.php
+++ config/services.php
@@ -14,6 +14,14 @@ return [
     |
     */
 
+    'midtrans' => [
+        'server_key'    => env('MIDTRANS_SERVER_KEY'),
+        'client_key'    => env('MIDTRANS_CLIENT_KEY'),
+        'is_production' => env('MIDTRANS_IS_PRODUCTION', false),
+        'is_sanitized'  => env('MIDTRANS_IS_SANITIZED', true),
+        'is_3ds'        => env('MIDTRANS_IS_3DS', true),
+    ],
+
     'postmark' => [
         'key' => env('POSTMARK_API_KEY'),
     ],
