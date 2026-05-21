db81acbc47276dab14723fa74c07b83dd136254a
ranggautama47 <ranggautama737@gmail.com>
2026-05-21 11:38:42 +0700

    feat: add CreateTransaction page for TransactionResource

---
+++ app/Filament/Resources/TransactionResource/Pages/CreateTransaction.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Filament\Resources\TransactionResource\Pages;
+
+use App\Filament\Resources\TransactionResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateTransaction extends CreateRecord
+{
+    protected static string $resource = TransactionResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function getCreatedNotificationTitle(): ?string
+    {
+        return 'Payment berhasil dibuat';
+    }
+}
